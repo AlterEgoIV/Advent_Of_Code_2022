29cecf76f298c7b3abe2cff069e82e2ee0000e87
AlterEgoIV <alterego01010@gmail.com>
2022-12-08 11:58:05 +0000

    AOC Day 2 Part 2

--- Day2/main.cpp
+++ Day2/main.cpp
@@ -7,15 +7,15 @@
 
 int32_t round(char a, char b)
 {
-	if(a == 'A' && b == 'X') return 4;
-	if(a == 'A' && b == 'Y') return 8;
-	if(a == 'A' && b == 'Z') return 3;
+	if(a == 'A' && b == 'X') return 3;
+	if(a == 'A' && b == 'Y') return 4;
+	if(a == 'A' && b == 'Z') return 8;
 	if(a == 'B' && b == 'X') return 1;
 	if(a == 'B' && b == 'Y') return 5;
 	if(a == 'B' && b == 'Z') return 9;
-	if(a == 'C' && b == 'X') return 7;
-	if(a == 'C' && b == 'Y') return 2;
-	if(a == 'C' && b == 'Z') return 6;
+	if(a == 'C' && b == 'X') return 2;
+	if(a == 'C' && b == 'Y') return 6;
+	if(a == 'C' && b == 'Z') return 7;
 
 	return 0;
 }
